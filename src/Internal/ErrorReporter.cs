fbf3001b53b0c8a318268f36d49826c799fba8fa
Sampo Kivistö <sampo.kivisto@visma.com>
2022-05-18 12:11:05 +0300

    Initial implementation based on Microsoft.AspNetCore.JsonPatch 6.0.5

---
+++ src/Internal/ErrorReporter.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using SystemTextJsonPatch.Exceptions;
+
+namespace SystemTextJsonPatch.Internal;
+
+internal static class ErrorReporter
+{
+    public static readonly Action<JsonPatchError> Default = (error) =>
+    {
+        throw new JsonPatchException(error);
+    };
+}

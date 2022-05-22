3e29e43ecd06973b34a0dd4377a3b781e1af9ed1
Sampo Kivistö <sampo.kivisto@visma.com>
2022-05-22 11:04:06 +0300

    Fixed publish script

--- publish.bat
+++ publish.bat
@@ -1,5 +1,3 @@
 dotnet build --configuration Release SystemTextJsonPatch/SystemTextJsonPatch.csproj /p:Configuration=Release
-dotnet build --configuration Release SystemTextJsonPatch.AspNet/SystemTextJsonPatch.AspNet.csproj /p:Configuration=Release
 
 dotnet pack --configuration Release SystemTextJsonPatch/SystemTextJsonPatch.csproj
-dotnet pack --configuration Release SystemTextJsonPatch.AspNet/SystemTextJsonPatch.AspNet.csproj

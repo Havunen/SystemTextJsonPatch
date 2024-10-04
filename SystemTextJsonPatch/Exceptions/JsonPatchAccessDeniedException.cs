7b2ff473ea1080a122d3f46f9124e3e37a124d04
AnderssonPeter <1308309+AnderssonPeter@users.noreply.github.com>
2024-10-04 09:04:16 +0200

    Improved JsonPatchAccessDeniedException

--- SystemTextJsonPatch/Exceptions/JsonPatchAccessDeniedException.cs
+++ SystemTextJsonPatch/Exceptions/JsonPatchAccessDeniedException.cs
@@ -11,11 +11,19 @@ namespace SystemTextJsonPatch.Exceptions
     public class JsonPatchAccessDeniedException : JsonPatchException
 #pragma warning restore CA1032 // Implement standard exception constructors
     {
+        public string Type { get; } = "N/A";
+        public string Property { get; } = "N/A";
         public JsonPatchAccessDeniedException(string message, Exception? innerException) : base(message, innerException)
         {
         }
 
-        public JsonPatchAccessDeniedException(PropertyInfo propertyInfo) : this($"Patch is not allowed to access the property {propertyInfo?.Name ?? "N/A"} on the type {propertyInfo?.DeclaringType?.Name ?? "N/A"}", null)
+        public JsonPatchAccessDeniedException(string type, string property) : this($"Patch is not allowed to access the property {property} on the type {type}", (Exception?)null)
+        {
+            this.Type = type;
+            this.Property = property;
+        }
+
+        public JsonPatchAccessDeniedException(PropertyInfo propertyInfo) : this(propertyInfo?.DeclaringType?.Name ?? "N/A", propertyInfo?.Name ?? "N/A")
         {
             
         }
